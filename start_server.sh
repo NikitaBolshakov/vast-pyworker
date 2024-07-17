15c83cc008c03e2d0c94338866aa7c3d99d81522
nikita.bolshakov <bolshakov.ns@yandex.ru>
2024-07-17 19:16:24 +0300

    fix():

--- start_server.sh
+++ start_server.sh
@@ -17,8 +17,8 @@ then
     echo "booting" | tee -a /root/debug.log
     mkdir /home/workspace
     cd /home/workspace
-    git clone https://github.com/vast-ai/vast-pyworker
-    
+    git clone https://github.com/NikitaBolshakov/vast-pyworker
+
     python3 -m venv /home/workspace/worker-env
     source /home/workspace/worker-env/bin/activate
 
